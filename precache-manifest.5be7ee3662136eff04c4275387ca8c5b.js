0fd8deec3725ffd451cdf79a4858bc730dbc5b81
vikas kumar <viomjeet@gmail.com>
2020-08-01 18:03:10 +0530

    Updates

---
+++ precache-manifest.5be7ee3662136eff04c4275387ca8c5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec432a26e17144b78e8c7cfa2f070b07",
+    "url": "/xtbox/index.html"
+  },
+  {
+    "revision": "f763b235891a7a99a67f",
+    "url": "/xtbox/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ed7051ae8314fc8e8ba5",
+    "url": "/xtbox/static/css/main.fed48702.chunk.css"
+  },
+  {
+    "revision": "f763b235891a7a99a67f",
+    "url": "/xtbox/static/js/2.a57d7964.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/xtbox/static/js/2.a57d7964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7051ae8314fc8e8ba5",
+    "url": "/xtbox/static/js/main.a50b7d28.chunk.js"
+  },
+  {
+    "revision": "6203e083eae492ccb242",
+    "url": "/xtbox/static/js/runtime-main.81adc1e5.js"
+  }
+]);
